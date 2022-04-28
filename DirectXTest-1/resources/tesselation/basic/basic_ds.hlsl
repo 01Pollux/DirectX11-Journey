fa829beb4afe561c4cd96b23c8a260fb82c5f41e
Yassine Sz <medrassa@hotmail.com>
2022-04-28 01:12:29 +0000

    Fix basic tesselation for triangle

--- DirectXTest-1/resources/tesselation/basic/basic_ds.hlsl
+++ DirectXTest-1/resources/tesselation/basic/basic_ds.hlsl
@@ -4,17 +4,18 @@
 [domain("tri")]
 PSInput main(
 	DSPatchOutput dsp_input,
-	float3 uv : SV_DomainLocation,
+	float3 uvw : SV_DomainLocation,
 	const OutputPatch<DSControlPoints, NUM_CONTROL_POINTS> dsc_input
 )
 {
 	PSInput ps_input;
 
-	float3 v1 = lerp(dsc_input[0].PosL, dsc_input[1].PosL, uv.x);
-	float3 v2 = lerp(dsc_input[2].PosL, dsc_input[0].PosL, uv.x);
-	float3 v = lerp(v1, v2, uv.y);
+	float3 v;
+	for (uint i = 0; i < NUM_CONTROL_POINTS; i++)
+		v += dsc_input[i].PosL * uvw[i % 3];
+	v /= 3.f;
 	
-	//v.y = .3f * (v.x * cos(v.z) + v.z * sin(v.x));
+	//v.z = 3.f * v.z * sin(v.x);
 	
 	ps_input.PosL = mul(mul(float4(v, 1.f), g_World), g_ViewProj);
 	
